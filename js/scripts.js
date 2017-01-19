cec9a3ae2719a765d03970a023d4fd330ebc1bb1
Erica Wright and Jason Brown <jason@>
2017-01-18 16:09:31 -0800

    fix dbl consenent rule.  Now translator can detect two beginning consenents

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,15 @@ for (var index = 0; index < vowel.length; index += 1);
 
 var latin = function(word) {
   var firstletter = word.charAt(0);
-  var restofword = word.substring(1, word.length);
-	if (firstletter === vowel[0] || firstletter === vowel[1] || firstletter === vowel[2] || firstletter === vowel[3] || firstletter === vowel[4]) {
+  var secondletter = word.charAt(1);
+  var restofword1 = word.substring(1, word.length);
+  var restofword2 = word.substring(2, word.length);
+  if (firstletter === vowel[0] || firstletter === vowel[1] || firstletter === vowel[2] || firstletter === vowel[3] || firstletter === vowel[4]) {
   return word + "ay";
-	} else {
-  return restofword + firstletter + "ay";
+  } else if ((firstletter != vowel[0] || firstletter != vowel[1] || firstletter != vowel[2] || firstletter != vowel[3] || firstletter != vowel[4]) && !(secondletter == vowel[0] || secondletter == vowel[1] || secondletter == vowel[2] || secondletter == vowel[3] || secondletter == vowel[4])) {
+  return restofword2 + firstletter + secondletter + "ay";
+  } else {
+  return restofword1 + firstletter + "ay";
   }
 };
 
