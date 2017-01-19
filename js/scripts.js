eb4ecef76d0eaf9b9605ee9f272ca82782a93799
Erica Wright and Jason Brown <erica@>
2017-01-18 16:39:49 -0800

    add notes to js to make sure this makes sense later

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,19 @@ var latin = function(word) {
   var restofword1 = word.substring(1, word.length);
   var restofword2 = word.substring(2, word.length);
   var restofword3 = word.substring(3, word.length);
+  //first statement - adds ay with words starting with a vowel
   if (firstletter === vowel[0] || firstletter === vowel[1] || firstletter === vowel[2] || firstletter === vowel[3] || firstletter === vowel[4]) {
   return word + "ay";
+  //if the word starts with squ it will place the squ at the end of the word but before ay
   } else if ((firstletter === "s") && (secondletter === "q") && (thirdletter === "u")) {
   return restofword3 + firstletter + secondletter + "u" + "ay";
+  // if the first two letters are both consenents, places ay at the end
   } else if ((firstletter != vowel[0] || firstletter != vowel[1] || firstletter != vowel[2] || firstletter != vowel[3] || firstletter != vowel[4]) && !(secondletter == vowel[0] || secondletter == vowel[1] || secondletter == vowel[2] || secondletter == vowel[3] || secondletter == vowel[4])) {
   return restofword2 + firstletter + secondletter + "ay";
+  // if the word starts with qu it will place qu at the end of the word, but before ay
   } else if ((firstletter === "q") && (secondletter === "u")) {
   return restofword2 + firstletter + secondletter + "ay";
+  // if all other statements are not true, this happens! yay!
   } else {
   return restofword1 + firstletter + "ay";
   }
