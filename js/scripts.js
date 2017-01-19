e96e66f8939eb7483c02d92d77ba4746fe224a8d
Erica Wright and Jason Brown <erica@>
2017-01-18 16:34:43 -0800

    add qu and squ rules to conditional statements

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,14 @@ for (var index = 0; index < vowel.length; index += 1);
 var latin = function(word) {
   var firstletter = word.charAt(0);
   var secondletter = word.charAt(1);
+  var thirdletter = word.charAt(2);
   var restofword1 = word.substring(1, word.length);
   var restofword2 = word.substring(2, word.length);
+  var restofword3 = word.substring(3, word.length);
   if (firstletter === vowel[0] || firstletter === vowel[1] || firstletter === vowel[2] || firstletter === vowel[3] || firstletter === vowel[4]) {
   return word + "ay";
+  } else if ((firstletter === "s") && (secondletter === "q") && (thirdletter === "u")) {
+  return restofword3 + firstletter + secondletter + "u" + "ay";
   } else if ((firstletter != vowel[0] || firstletter != vowel[1] || firstletter != vowel[2] || firstletter != vowel[3] || firstletter != vowel[4]) && !(secondletter == vowel[0] || secondletter == vowel[1] || secondletter == vowel[2] || secondletter == vowel[3] || secondletter == vowel[4])) {
   return restofword2 + firstletter + secondletter + "ay";
   } else if ((firstletter === "q") && (secondletter === "u")) {
